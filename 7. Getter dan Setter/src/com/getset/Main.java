c94ceefe630abdd16ced68f45ae6934d4e3fa617
rglrs <136968166+rglrs@users.noreply.github.com>
2024-06-29 17:28:05 +0700

    Add files via upload

---
+++ 7. Getter dan Setter/src/com/getset/Main.java
@@ -0,0 +1,79 @@
+package com.getset;
+
+class Data{
+    public int intPublic;
+    private int intPrivate;
+    private double doublePrivate;
+
+    public Data(){
+        this.intPublic = 0;
+        this.intPrivate = 200;
+    }
+
+    void display(){
+        System.out.println(this.intPublic);
+        System.out.println(this.intPrivate);
+        System.out.println(this.doublePrivate);
+    }
+
+    //getter
+    public int getIntPrivate(){
+        return this.intPrivate;
+    }
+
+    //setter
+    public void setDoublePrivate(double value){
+        this.doublePrivate = value;
+    }
+}
+
+//menggabungkan getter dan setter menggunakan perumpamaan menghitung seputar lingkaran
+class Lingkaran{
+    private double diameter;
+
+    Lingkaran(double diameter){
+        this.diameter = diameter;
+    }
+
+    //setter
+    public void setJari2(double jari2){
+        this.diameter = jari2 * 2;
+    }
+
+    //getter
+    public double getJari2(){
+        return this.diameter / 2;
+    }
+
+    //getter
+    public double getLuas(){
+        return 3.14 * diameter * diameter / 4;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Data object = new Data();
+
+        //public
+
+        //read dan write menggunakan public
+        object.intPublic = 100;//write
+        System.out.println("public : " + object.intPublic);//read
+
+        //read only (kita bisa menggunakan GETTER)
+        int angka = object.getIntPrivate();
+        System.out.println("getter : " + angka);
+
+        //write only (kita hanya bisa menulis menggunakan SETTER)
+        object.setDoublePrivate(0.05);
+        object.display();
+
+        //gabungkan read dan write dengan setter dan getter dari class lingkaran
+        Lingkaran object2 = new Lingkaran(10);
+        System.out.println("jari-jari : " + object2.getJari2());
+        object2.setJari2(14);
+        System.out.println("jari-jari : " + object2.getJari2());
+        System.out.println("Luas : " + object2.getLuas());
+    }
+}
